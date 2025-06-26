fe92d63c1b738d35b5dddb73b1180fc016849e48
Adam McKenna <adam-mckenna@outlook.com>
2025-06-26 16:22:14 +0200

    enable CORS for frontend

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,14 @@ import { ValidationPipe } from "@nestjs/common";
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+
+  const allowedOrigins = process.env.FRONTEND_URL.split(',');
+
+  app.enableCors({
+    origin: allowedOrigins,
+    credentials: true,
+  });
+
   await app.listen(3000);
   console.log(`Server is running on port 3000`);
 }
